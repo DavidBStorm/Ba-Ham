bf2a5ba6c2470f52d608666338e76ca48a5cb360
Goodarzi <goudarzi1212@gmail.com>
2024-05-20 15:49:20 +0330

    fixing UI on screen

--- app/src/main/java/com/task_baham/ui/composable/home/HomeScreen.kt
+++ app/src/main/java/com/task_baham/ui/composable/home/HomeScreen.kt
@@ -1,7 +1,5 @@
 package com.task_baham.ui.composable.home
 
-import android.graphics.BitmapFactory
-import android.media.ThumbnailUtils
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -9,44 +7,46 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import coil.ImageLoader
+import androidx.paging.LoadState
+import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
-import com.task_baham.ui.composable.universal.DisplayProgressbar
+import com.task_baham.ui.composable.universal.DisplayLoading
 import com.task_baham.util.GridItemSpan
 import com.task_baham.util.getHeightOfScreenInDp
 import com.task_baham.viewModel.home.HomeViewModel
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
 
 
 @Composable
 fun HomeScreen(homeViewModel: HomeViewModel) {
 
     LaunchedEffect(Unit) {
-        homeViewModel.getAllMedia()
+        homeViewModel.getMedia()
     }
-    val shouldShowDialog = homeViewModel.showProgressBar.collectAsState()
-    val media = homeViewModel.getMedia.collectAsState()
-    if (shouldShowDialog.value)
-        DisplayProgressbar(state = shouldShowDialog)
+    val mediaLazyItems = homeViewModel.getMedia().collectAsLazyPagingItems()
 
     Column(Modifier.fillMaxSize()) {
         Spacer(modifier = Modifier.height(150.dp))
-        LazyVerticalGrid(columns = GridCells.Fixed(GridItemSpan)) {
-            items(media.value.size, key = {
-                media.value[it]
-            }) {
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(GridItemSpan),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            items(mediaLazyItems.itemCount,
+                key = {
+                    mediaLazyItems.itemSnapshotList.items[it]
+                }
+            ) {
 
                 Column(
                     modifier = Modifier
@@ -54,12 +54,11 @@ fun HomeScreen(homeViewModel: HomeViewModel) {
                         .height(getHeightOfScreenInDp().div(5))
                         .background(Color.Green)
                 ) {
-                    val item = media.value[it]
+                    val item = mediaLazyItems.itemSnapshotList.items[it]
 
 
                     val model = ImageRequest.Builder(homeViewModel.getAppContext())
                         .data(item)
-                        .size(100)
                         .build()
 
                     AsyncImage(
@@ -70,12 +69,28 @@ fun HomeScreen(homeViewModel: HomeViewModel) {
                         contentDescription = "video thumbnail",
                         contentScale = ContentScale.Crop
                     )
-//                    Spacer(
-//                        modifier = Modifier
-//                            .fillMaxWidth()
-//                            .background(Color.Black)
-//                    )
                 }
+            }
+
+            when (mediaLazyItems.loadState.refresh) { //FIRST LOAD
+                is LoadState.Loading -> { // Loading UI
+                    item(span = { GridItemSpan(4) }) {
+                        DisplayLoading()
+                    }
+                }
+
+                else -> {}
+            }
+
+            when (mediaLazyItems.loadState.append) { // Pagination
+                is LoadState.Loading -> { // Pagination Loading UI
+                    item(span = { GridItemSpan(4) }) {
+                        DisplayLoading()
+
+                    }
+                }
+
+                else -> {}
             }
         }
     }
--- app/src/main/java/com/task_baham/util/Helper.kt
+++ app/src/main/java/com/task_baham/util/Helper.kt
@@ -1,8 +1,13 @@
 package com.task_baham.util
 
 import android.Manifest
+import android.app.Application
+import android.content.ContentUris
 import android.content.Context
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.MediaStore
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
@@ -40,3 +45,40 @@ fun requestPermission(
 fun getWidthOfScreenInDp() = LocalConfiguration.current.screenWidthDp.dp
 @Composable
 fun getHeightOfScreenInDp() = LocalConfiguration.current.screenHeightDp.dp
+
+ fun getAllMediaInStorage(application: Application): List<Uri> {
+    val allImages = mutableListOf<Uri>()
+
+    val imageProjection = arrayOf(
+        MediaStore.Images.Media._ID
+    )
+
+    val imageSortOrder = "${MediaStore.Images.Media.DATE_ADDED} DESC"
+
+    val cursor = application.contentResolver.query(
+        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+        imageProjection,
+        null,
+        null,
+        imageSortOrder
+    )
+
+    cursor.use {
+
+        if (cursor != null) {
+            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+            while (cursor.moveToNext()) {
+                allImages.add(
+                    ContentUris.withAppendedId(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        cursor.getLong(idColumn)
+                    )
+                )
+            }
+        } else {
+            Log.d("AddViewModel", "Cursor is null!")
+        }
+    }
+     Log.e("TAG", "getAllMediaInStorage: $allImages ")
+    return allImages
+}
