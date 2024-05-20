81cc30fdcc44fb1a0f7697acef6199d958dcce23
Goodarzi <goudarzi1212@gmail.com>
2024-05-20 14:08:15 +0330

    getting media from gallery

--- app/src/main/java/com/task_baham/viewModel/home/HomeViewModel.kt
+++ app/src/main/java/com/task_baham/viewModel/home/HomeViewModel.kt
@@ -1,21 +1,20 @@
 package com.task_baham.viewModel.home
 
+import android.content.ContentUris
 import android.content.Context
-import android.content.SharedPreferences
-import android.os.Build
+import android.net.Uri
+import android.provider.MediaStore
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.task_baham.MyApplication
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import java.util.Locale
 import javax.inject.Inject
 
 
@@ -24,15 +23,61 @@ class HomeViewModel @Inject constructor(
     private val application: MyApplication,
 ) : ViewModel() {
 
+    val showProgressBar: StateFlow<Boolean> get() = _showProgressBar
+    private val _showProgressBar = MutableStateFlow(true)
 
-    val versionCheck: StateFlow<String> get() = _versionCheck
-    private val _versionCheck = MutableStateFlow(String())
+    private val _getMedia = MutableStateFlow(listOf<Uri>())
+    val getMedia: StateFlow<List<Uri>> get() = _getMedia
+
 
     var job: Job? = null
     val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
         Log.e("TAG", "error main: ${throwable.message}")
     }
 
+    private fun getAllMediaInStorage(): List<Uri> {
+        val allImages = mutableListOf<Uri>()
+
+        val imageProjection = arrayOf(
+            MediaStore.Images.Media._ID
+        )
+
+        val imageSortOrder = "${MediaStore.Images.Media.DATE_ADDED} DESC"
+
+        val cursor = application.contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            imageProjection,
+            null,
+            null,
+            imageSortOrder
+        )
+
+        cursor.use {
+
+            if (cursor != null) {
+                val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+                while (cursor.moveToNext()) {
+                    allImages.add(
+                        ContentUris.withAppendedId(
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                            cursor.getLong(idColumn)
+                        )
+                    )
+                }
+            } else {
+                Log.d("AddViewModel", "Cursor is null!")
+            }
+        }
+        return allImages
+    }
+
+    fun getAllMedia() {
+        job = viewModelScope.launch(exceptionHandler) {
+            _getMedia.value = getAllMediaInStorage()
+            delay(1000)
+            _showProgressBar.value = false
+        }
+    }
 
     fun getAppContext(): Context = application.applicationContext
     fun getApp() = application
